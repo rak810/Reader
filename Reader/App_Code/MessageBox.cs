b18aebfee46210f32d3813b4de2826ea876c659f
rak810 <rak810@protonmail.ch>
2022-10-15 17:25:28 +0600

    remaining content added

---
+++ Reader/App_Code/MessageBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace Reader.App_Code
+{
+    public static class MessageBox
+    {
+        public static void Show(this Page Page, String Msg)
+        {
+            Page.ClientScript.RegisterStartupScript(
+               Page.GetType(),
+               "MessageBox",
+               "<script language='javascript'>alert('" + Msg + "');</script>"
+            );
+        }
+    }
+}
